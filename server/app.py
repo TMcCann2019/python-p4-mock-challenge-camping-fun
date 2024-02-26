3e16731836b4e7eea8908137dbd1acd80e42d388
Tim McCann <timothysmccann@gmail.com>
2024-02-26 13:06:14 -0500

    90 min

--- server/app.py
+++ server/app.py
@@ -25,5 +25,47 @@ db.init_app(app)
 def home():
     return ''
 
+@app.route('/campers')
+def get_campers():
+    campers = Camper.query.all()
+    camper_response = []
+    for camper in campers:
+        camp_response = {
+            'id': camper.id,
+            'name': camper.name,
+            'age': camper.age
+        }
+        camp_response.append(camper_response)
+    return make_response(jsonify(camper_response), 200)
+
+@app.route('/campers')  
+def post(self):
+    new_camper = Camper(
+        name = request.get_json()['name'],
+        age = request.get_json()['age']
+    )
+    db.session.add(new_camper)
+    db.session.commit()
+
+    response_dict = new_camper.to_dict()
+    return make_response(response_dict, 201)
+
+@app.route('/camper/<int:id>')
+def get(self, camper_id):
+    camper = Camper.query.filter_by(id=camper_id).first()
+    if not camper:
+        return {'error': 'Camper not found'}, 404
+    return camper.to_dict(), 200
+
+@app.route('/camper/<int:id>')
+def patch(self, camper_id):
+    camper = Camper.query.filter_by(id=camper_id).first()
+    if not camper:
+        return {'error': 'Camper not found'}, 404
+    camper.name = request.get_json()['name']
+    camper.age = request.get_json()['age']
+    db.session.commit()
+    return camper.to_dict(), 200
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -9,7 +9,7 @@ convention = {
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(constraint_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
+    "pk": "pk_%(table_name)s",
 }
 
 metadata = MetaData(naming_convention=convention)
@@ -18,52 +18,77 @@ db = SQLAlchemy(metadata=metadata)
 
 
 class Activity(db.Model, SerializerMixin):
-    __tablename__ = 'activities'
+    __tablename__ = "activities"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     difficulty = db.Column(db.Integer)
 
     # Add relationship
-    
+    signups = db.relationship("Signup", backref="activity", cascade="all, delete-orphan")
+
     # Add serialization rules
-    
+    serialize_rules = ("-signups.activitys",)
+
     def __repr__(self):
-        return f'<Activity {self.id}: {self.name}>'
+        return f"<Activity {self.id}: {self.name}>"
 
 
 class Camper(db.Model, SerializerMixin):
-    __tablename__ = 'campers'
+    __tablename__ = "campers"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
 
     # Add relationship
-    
+    signups = db.relationship("Signup", backref='camper', cascade="all, delete-orphan")
+
     # Add serialization rules
-    
+    serialize_rules = ("-signups.campers",)
+
     # Add validation
+    @validates("name")
+    def validate_camper_name(self, key, camper_name):
+        if camper_name is None:
+            raise ValueError("Camper name needs to be filled")
+        return camper_name
     
-    
+    @validates("age")
+    def validate_camper_age(self, key, age):
+        if age >= 8 and age <= 18:
+            return age
+        else:
+            raise ValueError("Camper age must be between 8 and 18")
+
     def __repr__(self):
-        return f'<Camper {self.id}: {self.name}>'
+        return f"<Camper {self.id}: {self.name}>"
 
 
 class Signup(db.Model, SerializerMixin):
-    __tablename__ = 'signups'
+    __tablename__ = "signups"
 
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.Integer)
+    camper_id = db.Column(db.Integer, db.ForeignKey("campers.id"))
+    activity_id = db.Column(db.Integer, db.ForeignKey("activities.id"))
 
     # Add relationships
-    
+    campers = db.relationship("Camper", backref="signup")
+    activitys = db.relationship("Activity", backref="signup")
+
     # Add serialization rules
-    
+    serialize_rules = ("campers.signups", "-activity.signups")
+
     # Add validation
-    
+    @validates("time")
+    def validate_time(self, key, time):
+        if time < 0 or time > 23:
+            raise ValueError("Time must be between 0 and 23")
+        return time
+
     def __repr__(self):
-        return f'<Signup {self.id}>'
+        return f"<Signup {self.id}>"
 
 
 # add any models you may need.
